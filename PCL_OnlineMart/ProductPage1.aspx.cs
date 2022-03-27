a5a342f2bcec8a4402f70bf7a05481bb3dabd488
sagar510 <sagarpal786t@gmail.com>
2022-03-28 00:28:32 +0530

    Database Insert Working

--- PCL_OnlineMart/ProductPage1.aspx.cs
+++ PCL_OnlineMart/ProductPage1.aspx.cs
@@ -19,7 +19,7 @@ namespace PCL_OnlineMart
             string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
             using (SqlConnection con = new SqlConnection(cs))
             {
-                SqlCommand cmd = new SqlCommand("select [Product_Image_Data] from [dbo].[Products] where [PID]=3490", con);
+                SqlCommand cmd = new SqlCommand("select [Product_Image_Data] from [dbo].[Products] where [PID]=751", con);
 
 
                 con.Open();
--- PCL_OnlineMart/bk.aspx.cs
+++ PCL_OnlineMart/bk.aspx.cs
@@ -10,6 +10,11 @@ namespace PCL_OnlineMart
 {
     public partial class bk : System.Web.UI.Page
     {
+        int AP, D,FP;
+        
+
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -17,6 +22,15 @@ namespace PCL_OnlineMart
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+
+            AP = Convert.ToInt32(TextBox4.Text);
+            D = Convert.ToInt32(TextBox5.Text);
+            FP = Convert.ToInt32(AP - (AP*((0.01) * D))   );
+
+
+
+
+
             HttpPostedFile postedFile = FileUpload1.PostedFile;
             string imagename = Path.GetFileName(postedFile.FileName);
 
@@ -30,33 +44,49 @@ namespace PCL_OnlineMart
             using (SqlConnection con = new SqlConnection(CS))
             {
 
-                SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Products]
-                    ([PID]
-                    ,[Product_Name]
-                    ,[Product_Image_Name]
-                  
-                    ,[Product_Description]
-                    ,[Actual_Price]
-                    ,[Disocount_Percent]
-                    
-                    ,[Delivery_Amount]
-                    ,[Rating]
-                    ,[Product_Category])
-                    VALUES
-                        ('" + TextBox1.Text + "', '" + TextBox2.Text + "','" + imagename + "','" + TextBox3.Text + "', '" + TextBox4.Text + "','" + TextBox5.Text + "','" + TextBox6.Text + "','" + TextBox7.Text + "','" + TextBox8.Text + "')", con );
-
-                SqlCommand cmd2 = new SqlCommand("spUploadImage",con);
+                
+                SqlCommand cmd2 = new SqlCommand("ProductPageData3",con);
                 cmd2.CommandType = CommandType.StoredProcedure;
 
-                SqlParameter paramImg = new SqlParameter()
-                {
-                    ParameterName = "@ImageData",
-                    Value = image
-                };
-                cmd2.Parameters.Add(paramImg);
+
+
+
+                SqlParameter PID = new SqlParameter() { ParameterName = "@PID", Value = TextBox1.Text };
+                cmd2.Parameters.Add(PID);
+
+                SqlParameter Product_Name = new SqlParameter(){ParameterName = "@Product_Name", Value = TextBox2.Text};
+                cmd2.Parameters.Add(Product_Name);
+
+                
+                SqlParameter Product_Image_Name = new SqlParameter() { ParameterName = "@Product_Image_Name", Value = imagename };
+                cmd2.Parameters.Add(Product_Image_Name);
+
+                SqlParameter Product_Image_Data = new SqlParameter() { ParameterName = "@Product_Image_Data", Value = image };
+                cmd2.Parameters.Add(Product_Image_Data);
+
+                SqlParameter Product_Description = new SqlParameter() { ParameterName = "@Product_Description", Value = TextBox3.Text };
+                cmd2.Parameters.Add(Product_Description);
+
+                SqlParameter Actual_Price = new SqlParameter() { ParameterName = "@Actual_Price", Value = TextBox4.Text };
+                cmd2.Parameters.Add(Actual_Price);
+
+                SqlParameter Disocount_Percent = new SqlParameter() { ParameterName = "@Disocount_Percent", Value = TextBox5.Text };
+                cmd2.Parameters.Add(Disocount_Percent);
+
+                SqlParameter @Final_Price = new SqlParameter() { ParameterName = "@Final_Price", Value = FP };
+                cmd2.Parameters.Add(@Final_Price);
+
+                SqlParameter Delivery_Amount = new SqlParameter() { ParameterName = "@Delivery_Amount", Value = TextBox6.Text };
+                cmd2.Parameters.Add(Delivery_Amount);
+
+                SqlParameter Rating = new SqlParameter() { ParameterName = "@Rating", Value = TextBox7.Text };
+                cmd2.Parameters.Add(Rating);
+
+                SqlParameter Product_Category = new SqlParameter() { ParameterName = "@Product_Category", Value = TextBox8.Text };
+                cmd2.Parameters.Add(Product_Category);
+
 
                 con.Open();
-                cmd.ExecuteNonQuery();
                 cmd2.ExecuteNonQuery();
                 con.Close();
                 
