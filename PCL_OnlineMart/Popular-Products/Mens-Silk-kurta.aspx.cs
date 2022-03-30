261e2211f3c3f374fc258ccb744095ee6c08685d
sagar510 <sagarpal786t@gmail.com>
2022-03-31 00:02:28 +0530

    Adding Actual database page

---
+++ PCL_OnlineMart/Popular-Products/Mens-Silk-kurta.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PCL_OnlineMart
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int PID = 10986;
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select [Product_Image_Data] from [dbo].[Products] where [PID]='" + PID + "'", con);
+
+
+                con.Open();
+                byte[] bytes = (byte[])cmd.ExecuteScalar();
+
+                string strbase64 = Convert.ToBase64String(bytes);
+                image1.ImageUrl = "data:Image/png;base64," + strbase64;
+
+
+
+
+                /*PName.InnerText = "Hello";
+                int a = 55;
+                PActualPrice.InnerText = a.ToString() + " % off";*/
+
+
+                SqlCommand cmd2 = new SqlCommand("select * from [dbo].[Products] where [PID]='" + PID + "'", con);
+
+                SqlDataReader sdr = cmd2.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    PName.InnerText = sdr["Product_Name"].ToString();
+                    PDescription.InnerText = sdr["Product_Description"].ToString();
+                    PActualPrice.InnerText = "₹ " + sdr["Actual_Price"].ToString() + ".00";
+                    Discount.InnerText = sdr["Disocount_Percent"].ToString() + " % off";
+                    PFinalPrice.InnerText = "₹ " + sdr["Final_Price"].ToString() + ".00";
+                }
+
+                con.Close();
+            }
+        }
+    }
+}
